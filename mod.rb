f701729d4dd3bfc8b9c1d32ffdfae5e4ad426a32
ramanujmukherjee <ramanujonlyme@gmail.com>
2013-07-04 18:08:51 +0530

    another ruby file added

---
+++ mod.rb
@@ -0,0 +1,17 @@
+module Test
+  def self.add(a, b)
+    a + b
+  end
+
+  def self.sub(a, b)
+    a - b
+  end
+
+  def self.mul(a, b)
+    a * b
+  end
+
+  def self.div(a, b)
+    a / b
+  end
+end
